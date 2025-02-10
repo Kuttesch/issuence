702d64980413226959f671eb6b372ab826da7b4e
Kuttesch <moritzkuttesch@outlook.de>
2025-02-10 23:43:33 +0100

    split up main.svelte

--- apps/frontend/src/lib/Main.svelte
+++ apps/frontend/src/lib/Main.svelte
@@ -1,19 +1,18 @@
 <script lang="ts">
     import {Badge, Hr, P} from 'flowbite-svelte';
-    import MD from './MD.svelte';
+    import MD from './main/MD.svelte';
     import { frontendVariables } from './store';
-
-    function formatDate(date_string: Date) {
-        let date = new Date(date_string);
-        if (date.toLocaleDateString == new Date().toLocaleDateString) {
-            return date.toLocaleTimeString();
-        } else {
-            return date.toDateString();
-        }
-    }
+    import Comments from './main/Comments.svelte';
+    import Titlebar from './main/Titlebar.svelte';
+    import TodoList from './main/TodoList.svelte';
 
 </script>
 
+<div class="w-3/4 min-w-170 h-full flex flex-row items-start justify-start overflow-y-scroll scrollbar-hide">
+    <div class="w-full min-h-full h-auto bg-background-secondary rounded-t-2xl shadow-2xl flex flex-col items-start justify-start p-12">
+        <Titlebar/>
+        <MD/>
         <TodoList/>
     </div>
+    <!-- <Comments/> -->
 </div>
