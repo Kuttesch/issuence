f6ffe6bd7e5a676e419b893bdff8a7589d236f67
Kuttesch <moritzkuttesch@outlook.de>
2025-02-10 10:17:29 +0100

    added marked and MD.svelte

--- apps/frontend/src/lib/Main.svelte
+++ apps/frontend/src/lib/Main.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    import {Badge, P} from 'flowbite-svelte';
+    import {Badge, Hr, P} from 'flowbite-svelte';
+    import MD from './MD.svelte';
     import { frontendVariables } from './store';
 
     function formatDate(date_string: Date) {
@@ -13,11 +14,11 @@
 
 </script>
 
-<div class="w-5/6 h-full flex flex-row items-center justify-center p-2">
-    <div class="w-full h-full bg-background-secondary rounded-2xl shadow-2xl flex flex-col items-center justify-start">
-        <div class="w-full h-2/5            rounded-t-2xl flex items-start justify-center">
-            <div class="w-full h-2/5        rounded-t-2xl flex flex-row items-start justify-start p-2">
-                <div class="w-3/5 h-full    text-2xl font-bold flex flex-col items-start justify-start ml-4">
+<div class="w-5/6 h-full flex flex-row items-center justify-center">
+    <div class="w-full h-full bg-background-secondary rounded-2xl shadow-2xl flex flex-col items-center justify-start p-12">
+        <!-- <div class="w-full h-2/5 bg-blue-500 rounded-t-2xl flex items-start justify-center"> -->
+            <div class="w-full h-1/5 rounded-t-2xl flex flex-row items-start justify-start">
+                <div class="w-3/5 h-full text-2xl font-bold flex flex-col items-start justify-start">
                     <div class="w-3/5 h-1/2 text-2xl font-bold flex flex-row items-center justify-start">{$frontendVariables.currentIssue.title}</div>
                     <div class="w-3/5 h-1/2 font-bold flex flex-row items-start justify-start">
                         <div class="w-1/3 h-full flex flex-row items-center justify-start">
@@ -34,13 +35,11 @@
                         </div>
                     </div>
                 </div>
-                <div class="w-2/5 h-full flex flex-row-reverse items-center justify-start p-2 mr-4">
+                <div class="w-2/5 h-full flex flex-row-reverse items-center justify-start">
                         <div class="text-xl bg-background rounded pl-3 pr-3 pb-1 pt-1"> Edit </div>
                 </div>
                 
             </div>
-        </div>
-
-
+        <MD text={$frontendVariables.currentIssue.description} />
     </div>
 </div>
