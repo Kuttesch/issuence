e413e2960d49724f2885f2a9422d9cf7b8d0dd7b
Kuttesch <moritzkuttesch@outlook.de>
2025-02-10 01:02:09 +0100

    made api more accessable

--- apps/backend/preload.ts
+++ apps/backend/preload.ts
@@ -2,14 +2,19 @@ import { contextBridge, ipcRenderer } from 'electron';
 import { Issue } from './data';
 
 contextBridge.exposeInMainWorld('electron', {
-        quitApp: (): Promise<void> => ipcRenderer.invoke('quitApp'),
-        maximizeWindow: (): Promise<boolean> => ipcRenderer.invoke('maximizeWindow'),
-        minimizeWindow: (): Promise<void> => ipcRenderer.invoke('minimizeWindow'),
-        getIssue: (id: number): Promise<Issue> => ipcRenderer.invoke('getIssue', id),
-        getNumberOfIssues: (): Promise<number> => ipcRenderer.invoke('getNumberOfIssues'),
-        getListOfAllIssueNames: (): Promise<string[]> => ipcRenderer.invoke('getListOfAllIssueNames'),
-        getNameOfIssue: (id: number): Promise<string> => ipcRenderer.invoke('getNameOfIssue', id),
-        addIssue: (issue: Issue): Promise<void> => ipcRenderer.invoke('addIssue', issue),
-        saveIssue: (issue: Issue): Promise<void> => ipcRenderer.invoke('saveIssue', issue),
-        removeIssue: (id: number): Promise<void> => ipcRenderer.invoke('removeIssue', id),
+        windowControls: {
+                quitApp: (): Promise<void> => ipcRenderer.invoke('quitApp'),
+                maximizeWindow: (): Promise<boolean> => ipcRenderer.invoke('maximizeWindow'),
+                minimizeWindow: (): Promise<void> => ipcRenderer.invoke('minimizeWindow'),
+        },
+        database: {
+                getIssue: (id: number): Promise<Issue> => ipcRenderer.invoke('getIssue', id),
+                getNumberOfIssues: (): Promise<number> => ipcRenderer.invoke('getNumberOfIssues'),
+                getListOfAllIssueNames: (): Promise<string[]> => ipcRenderer.invoke('getListOfAllIssueNames'),
+                getNameOfIssue: (id: number): Promise<string> => ipcRenderer.invoke('getNameOfIssue', id),
+                getIdOfIssue: (name: string): Promise<number> => ipcRenderer.invoke('getIdOfIssue', name),
+                addIssue: (issue: Issue): Promise<void> => ipcRenderer.invoke('addIssue', issue),
+                saveIssue: (issue: Issue): Promise<void> => ipcRenderer.invoke('saveIssue', issue),
+                removeIssue: (id: number): Promise<void> => ipcRenderer.invoke('removeIssue', id),
+        },
 });
