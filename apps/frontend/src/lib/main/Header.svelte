7e5e9384313bdc7e98ed6cb3cb029d232ba73888
Kuttesch <moritzkuttesch@outlook.de>
2025-02-15 22:10:11 +0100

    renamed some components for better clarity

--- apps/frontend/src/lib/main/Header.svelte
+++ apps/frontend/src/lib/main/Header.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-    import { Badge } from "flowbite-svelte";
+    import { Badge, Button } from "flowbite-svelte";
     import { frontendVariables } from "../store";
     import { CalendarEditOutline, CalendarWeekOutline, ClockOutline, EditOutline, EditSolid, TruckClockOutline, UserEditOutline } from "flowbite-svelte-icons";
+    import { Status } from "../../../data";
     let timeDate :boolean	= false;
     let edited: boolean = false;
     let shownDate: Date | null = null;
@@ -34,10 +35,16 @@
         shownDate = $frontendVariables.currentIssue.created;
     }
 
+    async function clickBadge(status: Status) {
+        console.log(status);
+        $frontendVariables.currentIssue.status = status;
+        await window.electron.database.saveIssue($frontendVariables.currentIssue);
+    }
+
 </script>
 
 
-<div class="w-full h-30 rounded-t-2xl flex flex-row items-start justify-start               ">
+<div class="w-full h-40 rounded-t-2xl flex flex-row items-start justify-start               ">
     <div class="w-6/7 h-full text-2xl font-bold flex flex-col items-start justify-start     ">
 <!-- Title -->
         <div class="w-5/5 h-1/2 text-3xl font-bold flex flex-row items-center justify-start ">
@@ -46,14 +53,22 @@
 <!-- Status -->
         <div class="w-2/5 h-1/2 font-bold flex flex-row items-start justify-start           ">
             <div class="min-w-25 w-1/4 h-full flex flex-row items-center justify-start              ">
-                {#if $frontendVariables.currentIssue.status === 'open'}
-                    <Badge color="green" class="text-xs w-20 aspect-[2/1]" rounded >open</Badge>
-                {:else if $frontendVariables.currentIssue.status === 'current'}
-                    <Badge color="blue" class="text-xs w-20 aspect-[2/1]" rounded>current</Badge>
-                {:else if $frontendVariables.currentIssue.status === 'done'}
-                    <Badge color="red" class="text-xs w-20 aspect-[2/1]" rounded>done</Badge>
-                {:else if $frontendVariables.currentIssue.status === 'later'}
-                    <Badge color="blue" class="text-xs w-20 aspect-[2/1]" rounded>later</Badge>
+                {#if $frontendVariables.currentIssue.status === Status.OPEN}
+                    <Button class="focus: ring-transparent p-0 m-0" on:click={() => clickBadge(Status.LATER)}>
+                        <Badge color="green" class="text-xs w-20 aspect-[2/1]" rounded>open</Badge>
+                    </Button>
+                {:else if $frontendVariables.currentIssue.status === Status.LATER}
+                    <Button class="focus: ring-transparent p-0 m-0" on:click={() => clickBadge(Status.CURRENT)}>
+                        <Badge color="blue" class="text-xs w-20 aspect-[2/1]" rounded>later</Badge>
+                    </Button>
+                {:else if $frontendVariables.currentIssue.status === Status.CURRENT}
+                    <Button class="focus: ring-transparent p-0 m-0" on:click={() => clickBadge(Status.DONE)}>
+                        <Badge color="indigo" class="text-xs w-20 aspect-[2/1]" rounded>current</Badge>
+                    </Button>
+                {:else if $frontendVariables.currentIssue.status === Status.DONE}
+                    <Button class="focus: ring-transparent p-0 m-0" on:click={() => clickBadge(Status.OPEN)}>
+                        <Badge color="red" class="text-xs w-20 aspect-[2/1]" rounded>done</Badge>
+                    </Button>
                 {/if}
             </div>
 <!-- Date -->
@@ -77,6 +92,6 @@
     </div>
 <!-- Edit -->
     <div class="w-1/7 h-full flex flex-row-reverse items-center justify-start">
-            <div class="text-xl bg-background rounded-xl pl-5 pr-5 pb-3 pt-3"> Edit </div>
+            <Button class="w-20 h-12 bg-primary text-text text-lg font-bold rounded-xl hover:scale-110 focus: ring-transparent"> Edit </Button>
     </div>
 </div>
