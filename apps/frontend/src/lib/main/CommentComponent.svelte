73990eece73c32a4f72935d9fb21a7ead85ede76
Kuttesch <moritzkuttesch@outlook.de>
2025-02-25 22:13:57 +0100

    reworked comment

--- apps/frontend/src/lib/main/CommentComponent.svelte
+++ apps/frontend/src/lib/main/CommentComponent.svelte
@@ -1,20 +1,16 @@
 <script lang="ts">
-    import { CalendarEditOutline, CalendarWeekOutline, ClockOutline, TrashBinOutline, UserEditOutline } from "flowbite-svelte-icons";
-    import {Timeline, TimelineConnector, TimelineItem, TimelineContent, TimelineDot, TimelineOppositeContent, TimelineSeparator} from "svelte-vertical-timeline";
+    import { TrashBinOutline } from "flowbite-svelte-icons";
+    import { TimelineConnector, TimelineItem, TimelineContent, TimelineDot, TimelineOppositeContent, TimelineSeparator } from "svelte-vertical-timeline";
     import { frontendVariables } from "../store";
-    import type {Comment} from "../../../../../shared/data";
     import { Button } from "flowbite-svelte";
     import { createEventDispatcher } from "svelte";
+    import type { Comment } from "../../../data";
 
     let dispatcher = createEventDispatcher();
 
     export let id: number;
     let commentIndex:number = id -1;
-
     let commentHover: boolean = false;
-
-    let comment: Comment;
-    $: comment = $frontendVariables.currentIssue.comments[commentIndex];
 
     function formatDate(date_string: Date | null): string {
         console.log(date_string);
@@ -32,17 +28,16 @@
         }
     }
 
-    async function deleteComment() {
-        console.log("Deleting comment");
-        $frontendVariables.currentIssue.comments.splice(commentIndex, 1);
-        await window.electron.database.saveIssue($frontendVariables.currentIssue);
-        dispatcher('deleteComment');
+    function deleteComment() {
+        dispatcher('deleteComment', id);
     }
 
 </script>
+
+{#if $frontendVariables.currentIssue.comments[commentIndex]}
     <TimelineItem>
         <TimelineOppositeContent>
-            <div class="w-[10vw] text-text text-lg font-normal">{formatDate(comment.created)}</div> <!--!TODO Styling-->
+            <div class="w-[10vw] text-text text-lg font-normal">{formatDate($frontendVariables.currentIssue.comments[commentIndex].created)}</div>
         </TimelineOppositeContent>
         <TimelineSeparator>
             <TimelineDot style="background-color: var(--color-accent);border-color: var(--color-accent);"/>
@@ -50,12 +45,13 @@
         </TimelineSeparator>
         <TimelineContent>
             <div class="min-h-10h-auto w-[30vw] text-text text-lg font-bold flex flex-row items-center justify-start" on:mouseenter={() => commentHover = true} on:mouseleave={() => commentHover = false} role="button" tabindex="0">
-            <div class="text-text text-lg">{comment.text}</div>
+            <div class="text-text text-lg">{$frontendVariables.currentIssue.comments[commentIndex].text}</div>
                 {#if commentHover}
-                    <Button class="p-0 m-0 focus: ring-transparent" on:click={() => deleteComment()}>
+                    <Button class="p-0 m-0 focus: ring-transparent" on:click={deleteComment}>
                         <TrashBinOutline class="w-6 h-6 ml-4 text-text text-lg" />
                     </Button>
                 {/if}
             </div>
         </TimelineContent>
     </TimelineItem>
+{/if}
