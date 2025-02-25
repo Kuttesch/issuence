2c714f94c45cf43bb056050d6836007adf591f60
Kuttesch <moritzkuttesch@outlook.de>
2025-02-25 22:13:33 +0100

    reworked todos

--- apps/frontend/src/lib/main/TodoItemComponent.svelte
+++ apps/frontend/src/lib/main/TodoItemComponent.svelte
@@ -4,48 +4,36 @@
     import { frontendVariables } from "../store";
     import { createEventDispatcher } from "svelte";
     
-    export let todo: any;
+    export let id: number;
 
     let dispatcher = createEventDispatcher();
 
     let todoHovered: boolean = false;
-    let todoIndex:number = todo.id -1;
-    let done: boolean;
-    $: todo = $frontendVariables.currentIssue.todoItems[todoIndex];
-    $: {
-        done = todo.done;
-        if (done === undefined) {
-            done = false;
-        }
-    }
+    let todoIndex:number = id - 1;
 
     function handlePress() {
-        todo.done = !todo.done;
-        $frontendVariables.currentIssue.todoItems[todoIndex] = todo;
-        window.electron.database.saveIssue($frontendVariables.currentIssue);
+        $frontendVariables.currentIssue.todoItems[todoIndex].done = !$frontendVariables.currentIssue.todoItems[todoIndex].done;
+        dispatcher('saveIssue');
     }
 
     async function deleteTodo() {
-        console.log("Deleting todo");
-        $frontendVariables.currentIssue.todoItems.splice(todoIndex, 1);
-        await window.electron.database.saveIssue($frontendVariables.currentIssue);
-        dispatcher('deleteTodo');
+        dispatcher('deleteTodo', id);
     }
 
 </script>
 <div class="w-full h-7 flex flex-col items-start justify-start m-2" on:mouseenter={() => todoHovered = true} on:mouseleave={() => todoHovered = false} role="button" tabindex="0">
-    <div class="w-1/2 h-7 flex items-center justify-start">
+    <div class="w-full h-7 flex items-center justify-start pr-6">
         <Button class="w-10 !p-2 !m-2 focus: ring-transparent" on:click={handlePress}>
             <div class="h-4 w-4 rounded-[100%] outline-2 outline-text flex items-center justify-center">
-                {#if done}
+                {#if $frontendVariables.currentIssue.todoItems[todoIndex].done}
                 <CheckOutline class="w-full h-full "/>
                 {/if}
             </div>
         </Button>
-        {#if done}
-            <div class="w-full h-full text-lg flex justify-start items-center font-bold line-through">{todo.text}</div>
+        {#if $frontendVariables.currentIssue.todoItems[todoIndex].done}
+            <div class="w-full h-full text-lg flex justify-start items-center font-bold line-through">{$frontendVariables.currentIssue.todoItems[todoIndex].text}</div>
         {:else}
-            <div class="w-full h-full text-lg flex justify-start items-center font-bold">{todo.text}</div>
+            <div class="w-full h-full text-lg flex justify-start items-center font-bold">{$frontendVariables.currentIssue.todoItems[todoIndex].text}</div>
         {/if}
         <Button class="p-0 m-0 focus: ring-transparent" on:click={deleteTodo}>
             <TrashBinOutline class="w-6 h-6 ml-4 text-text text-lg hover: scale-110" />
--- apps/frontend/src/lib/main/TodoList.svelte
+++ apps/frontend/src/lib/main/TodoList.svelte
@@ -4,11 +4,13 @@
     import TodoItemComponent from "./TodoItemComponent.svelte";
     import { CheckOutline, PlusOutline } from "flowbite-svelte-icons";
     import { TodoItem } from "../../../data";
+    import { createEventDispatcher } from "svelte";
     // let todos: TodoItem[] = [];
     let todos: any[] = [];
     let newTodo: string = "";
 
-    $: todos = $frontendVariables.currentIssue.todoItems;
+    let dispatcher = createEventDispatcher();
+
     function keydownHandler(event: KeyboardEvent) {
         if (event.key === "Enter") {
             addTodo();
@@ -17,33 +19,33 @@
 
     function addTodo() {
         if (newTodo !== "") {
-            let newId = todos.length + 1;
+            let newId = $frontendVariables.currentIssue.todoItems.length + 1;
             let todo = new TodoItem(newId, newTodo);
-            todos.push(todo);
-            $frontendVariables.currentIssue.todoItems = todos;
-            window.electron.database.saveIssue($frontendVariables.currentIssue);
+            $frontendVariables.currentIssue.todoItems.push(todo);
             newTodo = "";
+            dispatcher('saveCurrentIssue');
         }
     }
 
-    async function handleDeleteTodo() {
-        todos = $frontendVariables.currentIssue.todoItems;
+    async function handleDeleteTodo(event: CustomEvent<number>) {
+        $frontendVariables.currentIssue.todoItems = $frontendVariables.currentIssue.todoItems.filter(todo => todo.id !== event.detail);
+        dispatcher('saveCurrentIssue');
     }
 
 </script>
 
-<div class="w-full h-auto max-h-[50vh] min-h-30 rounded-2xl bg-background flex flex-col items-start justify-center overflow-y-scroll scrollbar-hide pt-4 pb-4 p-2 mt-2 mb-2">
+<div class="w-full h-auto max-h-[50vh] min-h-30 rounded-2xl bg-background flex flex-col items-start justify-center overflow-y-scroll scrollbar-hide pt-4 pb-4 p-2">
     {#each todos.sort((a, b) => Number(a.done) - Number(b.done)) as todo}
-        <TodoItemComponent todo={todo} on:deleteTodo={handleDeleteTodo}/>
+        <TodoItemComponent id={todo.id} on:deleteTodo={handleDeleteTodo}/>
     {/each}
-    <div class="w-1/2 h-7 flex flex-col items-start justify-start m-2">
+    <div class="w-full h-7 flex flex-col items-start justify-start m-2 pr-4">
         <div class="w-full h-7 flex items-center justify-start">
             <Button class="w-10 !p-2 !m-2 focus: ring-transparent" on:click={addTodo}>
                 <div class="h-4 w-4 rounded-[100%] outline-2 outline-text flex items-center justify-center">
                     <PlusOutline class="w-full h-full "/>
                 </div>
             </Button>
-            <Input color="" class="w-full h-25 rounded-2xl text-text text-lg font-bold p-0 m-0 hover: outline-0" placeholder="Add a todo" bind:value={newTodo} on:keydown={keydownHandler}/>
+            <Input color="" class="w-full h-25 rounded-2xl text-text text-lg font-bold p-0 m-0 hover: outline-0" placeholder="Add a todo..." bind:value={newTodo} on:keydown={keydownHandler}/>
             <Button class="w-10 !p-2 !m-2">
             </Button>
         </div>
