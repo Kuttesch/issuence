d07a1e8611860bfb7f3947e3868ad1504ebe57fb
Kuttesch <moritzkuttesch@outlook.de>
2025-03-02 16:02:07 +0100

    added ListComponent

---
+++ website/src/lib/ListComponent.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+    export let orientation: "left" | "right" = "left";
+
+
+
+</script>
+
+{#if orientation === "left"}
+<div class="w-full min-h-[25vh] h-auto flex flex-row bg-red-500">
+    <div class="w-1/2 min-h-full h-auto bg-green-400">
+        <slot name="picture"></slot>
+    </div>
+    <div class="w-1/2 min-h-full h-auto bg-blue-400">
+        <slot name="text" class="w-1/2 h-full bg-yellow-400"></slot>
+    </div>
+</div>
+{:else}
+<div class="w-full min-h-[25vh] h-auto flex flex-row bg-red-500">
+    <div class="w-1/2 min-h-full h-auto bg-blue-400">
+        <slot name="text" class="w-1/2 h-full bg-yellow-400"></slot>
+    </div>
+    <div class="w-1/2 min-h-full h-auto bg-green-400">
+        <slot name="picture"></slot>
+    </div>
+</div>
+{/if}
