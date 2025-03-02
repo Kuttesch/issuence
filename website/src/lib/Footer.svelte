1824fef8b4cc5259dfe02ba8b23c880449afae48
Kuttesch <moritzkuttesch@outlook.de>
2025-03-02 17:49:58 +0100

    Added theme switching

--- website/src/lib/Footer.svelte
+++ website/src/lib/Footer.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import { Button, P } from "flowbite-svelte";
-    import icon from "../assets/icon.png";
     import { GithubSolid } from "flowbite-svelte-icons";
     import { downloadLink } from "../stores";
 
@@ -11,6 +10,6 @@
 <div class="w-full h-10 flex justify-end items-center m-4 mr-16 gap-8 drop-shadow-header">
     <!-- <div class="text-lg hover:scale-110 cursor-pointer" on:click={() => openLink("https://www.github.com/Kuttesch/Issuence")}>Demo</div>
     <div class="text-lg hover:scale-110 cursor-pointer" on:click={() => openLink("https://www.github.com/Kuttesch/Issuence")}>Docs</div> -->
-    <Button class="text-lg text-text hover:scale-110 cursor-pointer focus:ring-0" on:click={() => openLink($downloadLink)}>Download</Button>
-    <GithubSolid class="h-full w-auto pt-2 pb-2 hover:scale-110 cursor-pointer" onclick={() => openLink("https://www.github.com/Kuttesch/Issuence")} />
+    <Button      class="text-text dark:text-dark-text text-lg p-0 pb-2 pt-2 hover:scale-110 cursor-pointer focus:ring-0" on:click={() => openLink($downloadLink)}>Download</Button>
+    <GithubSolid class="text-text dark:text-dark-text h-full w-auto p-0 pt-2 pb-2 hover:scale-110 cursor-pointer" onclick={() => openLink("https://www.github.com/Kuttesch/Issuence")} />
 </div>
--- website/src/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-// tailwind.config.ts
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  darkMode: "class", // Enables class-based dark mode
-};
-
-export default config;
